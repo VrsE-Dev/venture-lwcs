8ff3ada1d8b8afe26e88e70dda75c2844e22c6d8
jtd0015 <joshua.taylor.duncan.auburn@gmail.com>
2021-06-03 19:51:42 -0400

    Commit

---
+++ force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
@@ -0,0 +1,16 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+import createNewAuroraProject from '@salesforce/apex/AuroraSpikeController.createAuroraProject';
+
+import SITE_ID from '@salesforce/schema/Site.Id';
+import SITE_ACCOUNT_ID from '@salesforce/schema/Site.AccountId';
+import SITE_AURORA_PROJECT_ID from '@salesforce/schema/Site.Aurora_Project_Id__c';
+
+export default class AuroraProjectSiteButton extends NavigationMixin(LightningElement) {
+    @track site;
+
+    async createNewAuroraProject(){
+        await createNewAuroraProject(site);
+    }
+}
--- force-app/main/default/lwc/siteAuroraSystemDesign/siteAuroraSystemDesign.js
+++ force-app/main/default/lwc/siteAuroraSystemDesign/siteAuroraSystemDesign.js
@@ -7,7 +7,6 @@ import { getRecord } from 'lightning/uiRecordApi';
 
 import getProjectDesigns from '@salesforce/apex/AuroraSpikeController.getProjectDesigns';
 import getDesignSummary from '@salesforce/apex/AuroraSpikeController.getDesignSummary';
-import createNewAuroraProject from '@salesforce/apex/AuroraSpikeController.createAuroraProject';
 
 import getAllowedArrays from '@salesforce/apex/SystemOptimizerController.getAllowedArrays';
 
@@ -125,11 +124,6 @@ export default class AuroraSpike extends NavigationMixin(LightningElement) {
             this.showToast('success', `Successfully cloned pvArray ${pvArray.Id}`);
         });
     }
-
-    async createNewAuroraProject(){
-        await createNewAuroraProject(site);
-    }
-
     /**
      * Deletes existing allowed arrays and calls function to
      * create allowed arrays

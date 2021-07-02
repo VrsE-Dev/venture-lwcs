89e5dcfe2303871e33b09f8b851a9421507f3ec6
jtd0015 <joshua.taylor.duncan.auburn@gmail.com>
2021-07-01 21:38:45 -0400

    i hate this so much

--- force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
+++ force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
@@ -24,6 +24,30 @@ export default class AuroraProjectSiteButton extends NavigationMixin(LightningEl
         }
     )
 
+    getQuote({error, data}) {
+        if (data) {
+            console.log('data.fields:');
+            console.log(JSON.stringify(data.fields));
+
+            this.quote = {};
+            Object.keys(data.fields).map((v, k) => {
+                this.quote[v] = data.fields[v].value;
+            });
+
+            console.log('Aurora Spike Converted Quote:');
+            console.log(JSON.stringify(this.quote, undefined, 2));
+
+            if(!this.quote.Aurora_Project_Id__c) {
+                console.log('quote has no aurora id');
+                this.loadingDesignSummary = true;
+                this.noAuroraIdOnQuoteError = true;
+            }
+        } else if (error) {
+            console.log('Error getting quote data:');
+            console.log(JSON.stringify(error, undefined, 2));
+        }
+    }
+
     async createNewAuroraProject(){
         await createNewAuroraProject(this.site.Id, this.site.AccountId);
     }

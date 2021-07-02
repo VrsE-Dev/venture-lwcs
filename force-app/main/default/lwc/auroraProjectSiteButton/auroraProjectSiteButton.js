4877a11548f230e5cd09358caad3db6252f60b40
jtd0015 <joshua.taylor.duncan.auburn@gmail.com>
2021-07-01 21:16:29 -0400

    dumb

--- force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
+++ force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
@@ -5,8 +5,8 @@ import { getRecord } from 'lightning/uiRecordApi';
 import createNewAuroraProject from '@salesforce/apex/AuroraSpikeController.createAuroraProject';
 
 import SITE_ID from '@salesforce/schema/Site.Id';
-import SITE_ACCOUNT_ID from '@salesforce/schema/Site.Account__c.Id';
-import SITE_AURORA_PROJECT_ID from '@salesforce/schema/Site.Aurora_Project_Id__c';
+import SITE_ACCOUNT_ID from '@salesforce/schema/Site.Account.Id';
+import SITE_AURORA_PROJECT_ID from '@salesforce/schema/Site.Aurora_Project_Id';
 
 export default class AuroraProjectSiteButton extends NavigationMixin(LightningElement) {
     @track site;

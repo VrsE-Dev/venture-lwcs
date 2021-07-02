51caf3c655bbbcd7f16370817872d2a08f97348f
jtd0015 <joshua.taylor.duncan.auburn@gmail.com>
2021-07-01 21:00:34 -0400

    bullshit

--- force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
+++ force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
@@ -4,13 +4,25 @@ import { NavigationMixin } from 'lightning/navigation';
 import createNewAuroraProject from '@salesforce/apex/AuroraSpikeController.createAuroraProject';
 
 import SITE_ID from '@salesforce/schema/Site.Id';
-import SITE_ACCOUNT from '@salesforce/schema/Site.Account__c.Id';
+import SITE_ACCOUNT_ID from '@salesforce/schema/Site.AccountId';
 import SITE_AURORA_PROJECT_ID from '@salesforce/schema/Site.Aurora_Project_Id__c';
 
 export default class AuroraProjectSiteButton extends NavigationMixin(LightningElement) {
     @track site;
 
+    @wire(
+        getRecord,
+        {
+            recordId: '$recordId',
+            fields: [
+                SITE_ID,
+                SITE_ACCOUNT_ID,
+                SITE_AURORA_PROJECT_ID
+            ]
+        }
+    )
+
     async createNewAuroraProject(){
-        await createNewAuroraProject(this.site, this.site.SITE_ACCOUNT);
+        await createNewAuroraProject(this.site.Id, this.site.AccountId);
     }
 }

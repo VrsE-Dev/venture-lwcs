ef3aeb8a3f2e2b69773b0b9025e2ac0ceef65980
jtd0015 <joshua.taylor.duncan.auburn@gmail.com>
2021-07-01 21:19:54 -0400

    i hate my life

--- force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
+++ force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
@@ -5,8 +5,8 @@ import { getRecord } from 'lightning/uiRecordApi';
 import createNewAuroraProject from '@salesforce/apex/AuroraSpikeController.createAuroraProject';
 
 import SITE_ID from '@salesforce/schema/Site.Id';
-import SITE_ACCOUNT_ID from '@salesforce/schema/Site.Account.Id';
-import SITE_AURORA_PROJECT_ID from '@salesforce/schema/Site.Aurora_Project_Id';
+import SITE_ACCOUNTID from '@salesforce/schema/Site.AccountId';
+import SITE_AURORA_PROJECT_ID from '@salesforce/schema/Site.Aurora_Project_Id__c';
 
 export default class AuroraProjectSiteButton extends NavigationMixin(LightningElement) {
     @track site;
@@ -17,7 +17,7 @@ export default class AuroraProjectSiteButton extends NavigationMixin(LightningEl
             recordId: '$recordId',
             fields: [
                 SITE_ID,
-                SITE_ACCOUNT_ID,
+                SITE_ACCOUNTID,
                 SITE_AURORA_PROJECT_ID
             ]
         }

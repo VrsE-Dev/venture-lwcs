ebbb676759c0ef3b2b0d77549afc5b5a5ef02b54
jtd0015 <joshua.taylor.duncan.auburn@gmail.com>
2021-07-01 20:01:16 -0400

    minor tweaks

--- force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
+++ force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
@@ -4,13 +4,13 @@ import { NavigationMixin } from 'lightning/navigation';
 import createNewAuroraProject from '@salesforce/apex/AuroraSpikeController.createAuroraProject';
 
 import SITE_ID from '@salesforce/schema/Site.Id';
-import SITE_ACCOUNT from '@salesforce/schema/Site.Account__c';
+import SITE_ACCOUNT from '@salesforce/schema/Site.AccountId';
 import SITE_AURORA_PROJECT_ID from '@salesforce/schema/Site.Aurora_Project_Id__c';
 
 export default class AuroraProjectSiteButton extends NavigationMixin(LightningElement) {
     @track site;
 
     async createNewAuroraProject(){
-        await createNewAuroraProject(this.site, SITE_ACCOUNT);
+        await createNewAuroraProject(this.site.SITE_ID, this.site.SITE_ACCOUNT);
     }
 }

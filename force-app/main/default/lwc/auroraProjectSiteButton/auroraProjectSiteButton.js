5945f17027bcc25d171c68d5e482521f7fe50706
jtd0015 <joshua.taylor.duncan.auburn@gmail.com>
2021-07-01 21:40:26 -0400

    i hate everything

--- force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
+++ force-app/main/default/lwc/auroraProjectSiteButton/auroraProjectSiteButton.js
@@ -4,10 +4,6 @@ import { getRecord } from 'lightning/uiRecordApi';
 
 import createNewAuroraProject from '@salesforce/apex/AuroraSpikeController.createAuroraProject';
 
-import SITE_ID from '@salesforce/schema/Site.Id';
-import SITE_ACCOUNTID from '@salesforce/schema/Site.AccountId';
-import SITE_AURORA_PROJECT_ID from '@salesforce/schema/Site.Aurora_Project_Id__c';
-
 export default class AuroraProjectSiteButton extends NavigationMixin(LightningElement) {
     @track site;
     @api recordId;
@@ -23,30 +19,6 @@ export default class AuroraProjectSiteButton extends NavigationMixin(LightningEl
             ]
         }
     )
-
-    getQuote({error, data}) {
-        if (data) {
-            console.log('data.fields:');
-            console.log(JSON.stringify(data.fields));
-
-            this.quote = {};
-            Object.keys(data.fields).map((v, k) => {
-                this.quote[v] = data.fields[v].value;
-            });
-
-            console.log('Aurora Spike Converted Quote:');
-            console.log(JSON.stringify(this.quote, undefined, 2));
-
-            if(!this.quote.Aurora_Project_Id__c) {
-                console.log('quote has no aurora id');
-                this.loadingDesignSummary = true;
-                this.noAuroraIdOnQuoteError = true;
-            }
-        } else if (error) {
-            console.log('Error getting quote data:');
-            console.log(JSON.stringify(error, undefined, 2));
-        }
-    }
 
     async createNewAuroraProject(){
         await createNewAuroraProject(this.site.Id, this.site.AccountId);
